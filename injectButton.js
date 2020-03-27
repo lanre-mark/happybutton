64735c3eec20e5ce1777f907c4a4c938e117b23f
Matt Severyn <mseveryn@gmail.com>
2020-03-26 23:32:44 -0700

    Added manifest, image, and appended button --needs style and event

---
+++ injectButton.js
@@ -0,0 +1,8 @@
+const documentTime = document.querySelector("body");
+  console.log("command:", document);
+  console.log("command:", "It did it!!");
+  let happyButton = document.createElement('img');
+  let imageURL = chrome.extension.getURL("sourceImages/smileyButton.png");
+  //let imageURL = 'https://i.picsum.photos/id/545/200/300.jpg'
+  happyButton.src = imageURL;
+  documentTime.appendChild(happyButton);
