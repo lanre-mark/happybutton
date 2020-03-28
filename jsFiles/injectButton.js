65d29cb352571da71ae60c314bdc5e8dc1149b12
Matt Severyn <mseveryn@ThinkWell.corp>
2020-03-27 23:00:46 -0700

    added face position and animation

--- jsFiles/injectButton.js
+++ jsFiles/injectButton.js
@@ -1,3 +1,12 @@
+function injectStyles(url) {
+  var elem = document.createElement('link');
+  elem.rel = 'stylesheet';
+  elem.setAttribute('href', url);
+  document.body.appendChild(elem);
+}
+
+injectStyles(chrome.extension.getURL('style.css'));
+
 const documentTime = document.querySelector("body");
 
   console.log("command:", document);
@@ -5,12 +14,20 @@ const documentTime = document.querySelector("body");
 
   //create container and button
   const happyContainer = document.createElement('div');
+  happyContainer.classList.add('happyContainer');
   const happyButton = document.createElement('img');
   let imageURL = chrome.extension.getURL("sourceImages/smileyButton.png");
   happyButton.src = imageURL;
   happyButton.classList.add('happyButton');
 
+//create clickFace
+const clickFace = document.createElement('img');
+let imageURL2 = chrome.extension.getURL("sourceImages/clickFace2.png");
+clickFace.src = imageURL2;
+clickFace.classList.add('clickFace');
+
 //add them to the dom
   documentTime.appendChild(happyContainer);
   happyContainer.appendChild(happyButton);
+  happyContainer.appendChild(clickFace);
   
