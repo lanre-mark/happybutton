3ea46940b6de5a3082c8548a06b9a7503c872258
olanrewaju Makinde <lanre.developer@gmail.com>
2020-03-29 13:09:51 +0100

    annotating routes/index.js for the endpoint routes for happyButtonApp

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,40 @@
+/**
+ * All the endpoints exposed to the outside world on the happyButtonApp are defined in this file
+ * While they are defined, the routes defined here invokes a callback that defines what will be 
+ * done in the backend to meet a request's requirement.
+ * They endpoints include
+ *      /resources  i.e. https://alike-sore-hail.glitch.me/resources
+ *      /generate   i.e. https://alike-sore-hail.glitch.me/generate
+ *      /reset      i.e. https://alike-sore-hail.glitch.me/rest
+ *      /dump       i.e. https://alike-sore-hail.glitch.me/dump
+ *  In addition, these endpoint routes are then exported from this file and injected into the 
+ *  happyButtonApp by const index = require("./routes/index"); Line in server.js
+ */
+
+/**
+ * 
+ */
 var express = require("express");
+
+/**
+ * 
+ */
 var path = require("path");
+
+/**
+ * 
+ */
 var router = express.Router();
 
-var { database, happyResource } = require("../db");
+/**
+ * 
+ */
+var {
+    database
+} = require("../db");
 
 router.post("/resources", async(req, res, next) => {
-    // console.log(req.body);
     await database.set(req.body);
-    // console.log(database);
     return res.status(200).send({
         data: req.body,
         message: "Success"
@@ -32,7 +59,6 @@ router.get("/dump", async(req, res, next) => {
 
 router.get("/reset", async(req, res, next) => {
     const dumped = await database.reset();
-    // console.log(dumped);
     if (dumped.state) {
         return res.status(200).send({
             data: dumped.message,
@@ -48,12 +74,7 @@ router.get("/reset", async(req, res, next) => {
 
 router.get("/generate", async(req, res, next) => {
     const genRate = await database.generate();
-    // console.log(genRate);
-    // console.log(database);
     return res.status(200).send(genRate.message);
-    // happyResource.then(rsp => {
-    //     console.log(rsp);
-    // });
 });
 
 module.exports = router;
