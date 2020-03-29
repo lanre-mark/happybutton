0e82bbe7a4be51ea2a8f43d46b25de327526c850
olanrewaju Makinde <lanre.developer@gmail.com>
2020-03-29 12:56:31 +0100

    more annotation of the concepts used in server.js for ease of comprehension by collaborators

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-/** 
- *   The www file is the entry point of this project. This is file creates the Node.js server
- *   using hte http module in node. 
- *   It setups up debugging and loads the entire app using the server.js as the nucleus/proxy
- *   Declares the port in which the server runs and add an error listerner on the routes
- * 
- * 
- * 
- */
-
 /**
  * Loads the most common module on node. The express modele added to the project from 
  * npm using npm install express on the command line of the project's root. 
@@ -94,6 +84,8 @@ happyButtonApp.use(bodyParser.json());
  * Request activities such as the url, files served, time details, etc are logged onto the screen
  * or on anyother provided in the setup of morgan's configuration. For the purpose of this project
  * we used the default, loggin to console.
+ * NOTE: These logging can be limited to the enviroment of implementation, in this case 'dev' environment
+ * as passed as a parameter into the logger invocation
  */
 happyButtonApp.use(logger("dev"));
 
@@ -106,7 +98,15 @@ happyButtonApp.use(express.static("public"));
 
 
 /**
+ *  We encountered a problem relating to CORS being enabled and the fact that most of the request 
+ *  from the extension not getting across to our backend on the cloud host as a result of CORS
+ *  (Cross Origin access issues) 
+ *  After a lot of research, installing cors module and including it as a middleware OR 
+ *  adding the basic setup manually as a middleware helped avoided this challenge
  * 
+ * For more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+ *           https://flaviocopes.com/express-cors/
+ *           https://medium.com/@alexishevia/using-cors-in-express-cac7e29b005b
  */
 // Enable CORS from client-side
 happyButtonApp.use(function(req, res, next) {
@@ -134,7 +134,10 @@ happyButtonApp.use(function(req, res, next) {
 
 
 /**
- * 
+ *  This middleware is propagated when there is an error in any request that comes into
+ *  the backend app. Errors could be from the responses in the app on any endpoint
+ *  or from the bodyparsers, loggers, etc
+ * NOTE: this was limited only to our development enviroment as indicated.
  */
 // error handler
 happyButtonApp.use(function(err, req, res, next) {
@@ -143,26 +146,44 @@ happyButtonApp.use(function(err, req, res, next) {
     res.locals.error = req.app.get("env") === "development" ? err : {};
     // render the error page
     res.status(err.status || 500);
+    /**
+     * below code will go ahead to load an error page globally int he application
+     * but this was disabled just to keep things simple. It would have rendered the error page
+     */
     // res.render("error");
 });
 
 
-
 /**
- * 
+ * Mounting the endpoint for main domain to the happyButtonApp application 
+ * i.e.the main domain being the domain used to host the backend.
+ *  In our case, we used a platform glitch.com to host the backend and were given the domain
+ *  https://alike-sore-hail.glitch.me
+ *  When you go to the browser and hit this domain, this endpoint is called as it has no other
+ *  route.
  */
 happyButtonApp.get("/", function(req, res, next) {
+    /**
+     * This route serves/returns the index.html in the public folder for any request on the 
+     * main domain - https://alike-sore-hail.glitch.me/
+     */
     res.sendFile(`${__dirname}/views/index.html`);
 });
 
 
 /**
- * 
+ * Mounting the endpoint all other routes on to the happyButtonApp application
+ * These routes will include every one added to the after a /
+ * e.g https: //alike-sore-hail.glitch.me/whoareyou where "whoareyou" is a route
+ * These routes are all defined on the index label, the index label contains all routes
+ * exported from the ./routes/index.js
  */
 happyButtonApp.use("/", index);
 
 
 /**
- * 
+ * the happyButtonApp label is now an object contains all the methods defined above
+ * the methods consists of middlewares, routes, exposed modules __proto__ etc
+ * You can print the object to have an idea of what it looks like
  */
 module.exports = happyButtonApp;
